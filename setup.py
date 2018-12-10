269bb40a189991fbd32bc431cdc0501b1cf25895
ed0zer <ed0zer@gmail.com>
2018-12-10 20:04:32 +0200

    * Fix parsing of requirements to also support newer versions of pip * Bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import uuid
 from setuptools import setup
 import os
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 reqs_file = os.path.join(BASE_DIR, 'requirements.txt')
@@ -13,17 +17,18 @@ install_reqs = parse_requirements(reqs_file, session=uuid.uuid1())
 
 setup(
     name="pyfoobar2k",
-    version="0.2.3",
+    version="0.2.4",
     author="Edo Ozer",
     author_email="ed0zer@gmail.com",
-    description="foobar2000, foo_httpcontrol",
+    description="control foobar2000 via foo_httpcontrol",
     license='MIT',
     keywords="pyfoobar2k, foo_httpcontrol, foobar2000",
     install_requires=["requests"],
     packages=['pyfoobar2k'],
     url="https://bitbucket.org/ed0zer/pyfoobar2k",
-    long_description=read('README'),
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
     ])
